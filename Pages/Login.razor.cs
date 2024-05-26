ea886a7cbbc177c2d56065e29acdff2d0f37c412
Ayoub El haoussi <aelhaoussi@sqli.com>
2024-05-26 19:04:07 +0100

    intial

---
+++ Pages/Login.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using TelerikBlazorDemos.Models.Form;
+
+namespace APEX.Pages
+{
+    public partial class Login
+    {
+        private User TestUser = new User();
+
+        public bool HidePassword { get; set; } = true;
+        public string Icon { get; set; } = "k-icon k-svg-icon k-svg-i-eye-slash";
+
+
+        public async Task RevealPassword()
+        {
+            HidePassword = false;
+            await Task.Delay(1700);
+            HidePassword = true;
+
+
+        }
+
+        private void HandleValidSubmit()
+        {
+            // Handle valid form submission
+        }
+
+        public class User
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
+
