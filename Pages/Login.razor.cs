05a2fc0eb915201a52e68b2d6b8d170a183e9ace
Ayoub El haoussi <aelhaoussi@sqli.com>
2024-05-27 10:23:17 +0100

    FluentValidation

--- Pages/Login.razor.cs
+++ Pages/Login.razor.cs
@@ -25,11 +25,7 @@ namespace APEX.Pages
             // Handle valid form submission
         }
 
-        public class User
-        {
-            public string Email { get; set; }
-            public string Password { get; set; }
-        }
+       
     }
 }
 
